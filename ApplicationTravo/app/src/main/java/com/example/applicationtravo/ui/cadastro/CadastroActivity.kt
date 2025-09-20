74f85333f4ceb359aad4821d1cb967cc9be921e8
Mayara Medino <medino.dev@gmail.com>
2025-09-20 11:29:18 -0300

    Conclusão da Tela de Login

---
+++ ApplicationTravo/app/src/main/java/com/example/applicationtravo/ui/cadastro/CadastroActivity.kt
@@ -0,0 +1,17 @@
+package com.example.applicationtravo.ui.cadastro
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.applicationtravo.R
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cadastro)
+
+    }
+}
--- ApplicationTravo/app/src/main/java/com/example/applicationtravo/ui/login/LoginActivity.kt
+++ ApplicationTravo/app/src/main/java/com/example/applicationtravo/ui/login/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.applicationtravo.ui.login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.applicationtravo.R
+import com.example.applicationtravo.ui.cadastro.CadastroActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +16,12 @@ class LoginActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
 
+        val btnCadastrar = findViewById<Button>(R.id.btnCadastrar)
+
+        btnCadastrar.setOnClickListener{
+
+            val intent = Intent(this, CadastroActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
