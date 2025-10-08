a39ecb24ceafe150646bfad44aa8eee66889c75c
Mayara Medino <medino.dev@gmail.com>
2025-10-08 10:02:41 -0300

    conflito

--- ApplicationTravo/app/src/main/java/com/example/applicationtravo/ui/configuracoes/Configuracoes.kt
+++ ApplicationTravo/app/src/main/java/com/example/applicationtravo/ui/configuracoes/Configuracoes.kt
@@ -10,12 +10,14 @@ import com.example.applicationtravo.R
 class Configuracoes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_configuracoes)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        val btnNotificacao = findViewById<Button>(R.id.btnNotificacao)
+        val btnAcessibilidade = findViewById<Button>(R.id.btnAcessibilidade)
+        val btnSuporte = findViewById<Button>(R.id.btnSuporte)
+        val btnInformacoes = findViewById<Button>(R.id.btnInformacoes)
+        val btnMenu = findViewById<TextView>(R.id.btnMenu)
+        val btnHome = findViewById<Button>(R.id.btnHome)
+        val btnConfiguracoes = findViewById<Button>(R.id.btnConfiguracoes)
+        val btnDescontos = findViewById<Button>(R.id.btnDescontos)
     }
 }
